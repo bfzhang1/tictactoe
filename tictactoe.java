93f4b57404007d9bf42e0bf9b53f19007245bc5a
Brandon Zhang <31413216+bfzhang1@users.noreply.github.com>
2017-10-18 15:20:19 -0500

    TicTacToe #3.5
    Changed a small typo.

--- tictactoe.java
+++ tictactoe.java
@@ -100,7 +100,7 @@ public class tictactoe{
             return;
         }
         if(board[0][2] == board[2][0] && board[1][1] != 0){
-            board[1][1] = 0;
+            board[1][1] = 2;
             return;
         }
         
